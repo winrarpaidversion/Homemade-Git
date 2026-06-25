71e65e59b48f264e235eb4a23ae9d77b2468480f
blackout0346 <zloianton087@gmail.com>
2026-06-25 19:52:55 +0800

    feat: added ofd for new repository

---
+++ src/HomemadeGit.Desktop/ViewModel/DialogService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomemadeGit.Desktop.ViewModel
+{
+    public interface IDialogService
+    {
+
+        string SelectFolder();
+    }
+    public class DialogService : IDialogService
+    {
+        public string SelectFolder()
+        {
+            var dialog = new OpenFolderDialog
+            {
+                Title = "Выберите папку для инициализации репозитория"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                return dialog.FolderName;
+            }
+            return null;
+        }
+    }
+}
--- src/HomemadeGit.Desktop/ViewModel/GitViewModel.cs
+++ src/HomemadeGit.Desktop/ViewModel/GitViewModel.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HomemadeGit.Desktop.ViewModel
 {
+
     public partial class GitViewModel : ViewModelBase
     {
-    }
+        [ObservableProperty]
+        private string _title;
+        [ObservableProperty]
+        private string _description;
+        [ObservableProperty]
+        private string _path;
+        private DialogService DialogService;
+        public GitViewModel()
+        {
+            DialogService = new DialogService();
+        }
+        [RelayCommand]
+        private void PushCommit()
+        {
+            if (string.IsNullOrWhiteSpace(Title) || string.IsNullOrWhiteSpace(Description)) return;
+
+        }
+        [RelayCommand]
+        private void NewRepository()
+        {
+
+            DialogService.SelectFolder();
+
+
+        }
+        }
+
+    
 }
+
