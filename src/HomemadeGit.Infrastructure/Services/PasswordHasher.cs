ba162d131529540577987d1246556c41e214e80a
winrarpaidversion <leadion48@gmail.com>
2026-06-26 20:22:17 +0800

    feat: pipis
    TO-DO: AuthService!!!

---
+++ src/HomemadeGit.Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using BCrypt.Net;
+using HomemadeGit.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomemadeGit.Infrastructure.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        const int workFactor = 11;
+        public async Task<string> HashPasswordAsync(string password)
+        {
+            return await Task.Run(() => BCrypt.Net.BCrypt.EnhancedHashPassword(password, workFactor));
+        }
+
+        public async Task<bool> VerifyPasswordAsync(string password, string hashedPassword)
+        {
+            return await Task.Run(() => BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword));
+        }
+    }
+}
