b4b6f5cb13eb81e8c75e6d624abb6bdb97cbb494
winrarpaidversion <leadion48@gmail.com>
2026-06-28 21:50:25 +0800

    feat: add commit DTOs and interfaces

---
+++ src/HomemadeGit.Core/DTOs/Commits/CommitFileResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomemadeGit.Core.DTOs.Commits
+{
+    public class CommitFileResponse
+    {
+        public string Path { get; set; } = string.Empty;
+        public string BlobHash { get; set; } = string.Empty;
+        public long Size { get; set; }
+    }
+}
---
+++ src/HomemadeGit.Core/Interfaces/ICommitService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomemadeGit.Core.DTOs.Commits;
+
+namespace HomemadeGit.Core.Interfaces
+{
+    public interface ICommitService
+    {
+        Task<CommitResponse> CreateCommitAsync(int userId, int repositoryId, CreateCommitRequest request);
+
+        Task<List<CommitListItemResponse>> GetRepositoryCommitsAsync(int userId, int repositoryId);
+
+        Task<CommitResponse> GetCommitByIdAsync(int userId, int commitId);
+    }
+}
